9500386ce9decb258a5a062384fb3d6abf1101b0
Mayokun <mayorbofficial@gmail.com>
2025-10-12 17:18:35 +0100

    Fix start.sh for Render web service

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+# #!/bin/bash
+# python manage.py migrate --noinput
+# python manage.py collectstatic --noinput
+# gunicorn TelegramBot.wsgi:application &
+# python telegram_bot.py
+
+#!/usr/bin/env bash
+set -o errexit
+
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn TelegramBot.wsgi:application &
-python telegram_bot.py
+
+# Start the web server and bind to Render's provided port
+gunicorn TelegramBot.wsgi:application --bind 0.0.0.0:$PORT
